644488d8551982a3dbdab70e9634779c7d3c3256
James Haggerty <james.haggerty@gmail.com>
2019-05-02 20:20:18 +1000

    Flatten out those classes
    Also, disturbing.

--- pif.py
+++ pif.py
@@ -19,11 +19,11 @@ GRAMMAR = """
 Program: Block;
 Block: paragraphs*=Paragraph;
 Paragraph: /(?m)\n*/ lines+=Line /(?m)\n*/;
-Line: assignment=Assignment? ( expr=Sentence | expr=IfStatement | expr=WhileStatement | expr=ForStatement ) /(?m)\n/;
+Line: assignment=Assignment? ( expr=Sentence | expr=If | expr=While | expr=For ) /(?m)\n/;
 Sentence: instructions+=Instruction;
-IfStatement: 'if' condition=Sentence 'do' block=Block ('elif' elif_conditions=Sentence 'do' elif_blocks=Block)* ('else' else_block=Block)? 'end';
-WhileStatement: 'while' condition=Sentence 'do' block=Block 'end';
-ForStatement: 'for' (var=MyID 'in')? sentence=Sentence 'do' block=Block 'end';
+If: 'if' condition=Sentence 'do' block=Block ('elif' elif_conditions=Sentence 'do' elif_blocks=Block)* ('else' else_block=Block)? 'end';
+While: 'while' condition=Sentence 'do' block=Block 'end';
+For: 'for' (var=MyID 'in')? sentence=Sentence 'do' block=Block 'end';
 Instruction: Pushable | '.';
 Assignment: var=MyID '=';
 Pushable: function=Function | var=MyID | rawval=RawVal | expr=Expression;
@@ -40,184 +40,136 @@ MyID: !Keyword ID;
 #Type: Types | ID;
 
 
-class Types:
-    def __init__(self, parent, varsWithType=None):
-        self.varsWithType = varsWithType
-
-    def eval(self, c):
-        return {entry.name: c.get_name(entry.type) for entry in self.varsWithType}
-
-
-class Pushable:
-    def __init__(self, parent, var, rawval, expr, function):
-        self.var = var
-        self.rawval = rawval
-        self.expr = expr
-        self.function = function
-
-    def eval(self, c):
-        if self.var:
-            return c.get_name(self.var)
-        elif self.expr:
-            return self.expr.e.eval(c)
-        elif self.function:
-            logging.debug(c)
-            self.function.add_context(c)
-            return self.function
-        elif hasattr(self.rawval, 'sentences'):
-            return [s.eval(c) for s in self.rawval.sentences]
-        elif hasattr(self.rawval, 'keys'):
-            return {k.eval(c): v.eval(c) for (k, v) in zip(self.rawval.keys, self.rawval.values)}
-        else:
-            return self.rawval
-
-
-class Function:
-    def __init__(self, parent=None, args=None, block=None, builtin=None, arg_dict=None):
-        self.parent = parent
-        self.args = args
-        self.block = block
-        self.builtin = builtin
-        self.arg_dict = arg_dict
-
-    def add_context(self, c):
-        self.c = c
-        self.arg_dict = self.args.eval(c)
-
-    def eval(self, s_c):
-        if self.block:
-            return self.block.eval(self.c.new_function(s_c, self.arg_dict))
-        elif self.builtin:
-            args = [s_c.pop_type(t) for (n, t) in self.arg_dict.items()]
-            return self.builtin(*args)
-
-
-class IfStatement:
-    def __init__(self, parent=None, condition=None, block=None, elif_conditions=None, elif_blocks=None, else_block=None):
-        self.parent = parent
-        self.condition = condition
-        self.block = block
-        self.else_block = else_block
-        self.elif_blocks = elif_blocks
-        self.elif_conditions = elif_conditions
-
-    def eval(self, c):
-        zipped = zip([self.condition] + (self.elif_conditions or []),
-                     [self.block] + (self.elif_blocks or []))
-        for (cond, block) in zipped:
-            res = cond.eval(c)
-            debug(self, 'evaluating condition')
-            if res is None:
-                raise Exception('must have a result')
-            elif res:
-                debug(self, 'block eval')
-                return block.eval(c)
-
-        if self.else_block:
-            return self.else_block.eval(c)
-
-        return None
-
-
-class WhileStatement:
-    def __init__(self, parent=None, condition=None, block=None):
-        self.parent = parent
-        self.condition = condition
-        self.block = block
-
-    def eval(self, c):
-        last_eval = None
-        while True:
-            res = self.condition.eval(c)
-            if res is None:
-                raise Exception('must have a result')
-            elif res:
-                last_eval = self.block.eval(c)
-            else:
-                break
-        return last_eval
-
-
-class ForStatement:
-    def __init__(self, parent=None, var=None, sentence=None, block=None):
-        self.parent = parent
-        self.var = var
-        self.sentence = sentence
-        self.block = block
-
-    def eval(self, c):
-        last_eval = None
-        for elem in self.sentence.eval(c):
-            if not self.var:
-                c.push_type(elem)
-            elif self.var and self.var != '_':
-                c.push_name(self.var, elem)
-            last_eval = self.block.eval(c)
-        return last_eval
-
-
-class Sentence:
-    def __init__(self, parent=None, instructions=None):
-        self.parent = parent
-        self.instructions = instructions
-
-    def eval(self, c):
-        debug(self)
-        s_c = Context(c)
-        for instruction in self.instructions:
-            if instruction == '.':
-                logging.debug('before func: %s', s_c)
-                val = s_c.pop_type(Function).eval(s_c)
-                if val is not None:
-                    s_c.push_type(val)
-                logging.debug('after func: %s', s_c)
-            else:
-                s_c.push_type(instruction.eval(c))
-
-        return s_c.result()
-
-
-class Block:
-    def __init__(self, parent=None, paragraphs=None):
-        self.paragraphs = paragraphs
-
-    def eval(self, c):
-        for paragraph in self.paragraphs:
-            debug(paragraph)
-            for line in paragraph.lines:
-                debug(line)
-                try:
-                    res = line.expr.eval(c)
-                    if res is None:
-                        assert not line.assignment
-                    elif isinstance(line.expr, Sentence):
-                        if not line.assignment:
-                            c.push_type(res)
-                        elif line.assignment.var != '_':
-                            c.push_name(line.assignment.var, res)
-                    elif line.assignment:
-                        # we need to dump what's been pushed, as it was wrong.
-                        # TODO - shouldn't push it in the first place?
-                        c.pop_type(res)
-                        if line.assignment.var != '_':
-                            c.push_name(line.assignment.var, res)
-                except Exception as e:
-                    error(line.expr, e)
-
-            try:
-                c.result()  # validate each paragraph has at most one result
-            except Exception as e:
-                error(paragraph, e)
-
-        logging.debug('function result %s', c.result())
-        return c.result()
-
-
 model = metamodel_from_str(
   GRAMMAR,
-  classes=[Pushable, Block, Sentence, WhileStatement, IfStatement, ForStatement, Function, Types] + expression.grammar_classes,
+  classes=expression.grammar_classes,
   ws=' '
 )
+
+def function_eval(self, s_c):
+    return self.block.eval(self.c.new_function_context(s_c, self.arg_dict))
+
+
+def if_eval(self, c):
+    zipped = zip([self.condition] + (self.elif_conditions or []),
+                 [self.block] + (self.elif_blocks or []))
+    for (cond, block) in zipped:
+        res = cond.eval(c)
+        debug(self, 'evaluating condition')
+        if res is None:
+            raise Exception('must have a result')
+        elif res:
+            debug(self, 'block eval')
+            return block.eval(c)
+
+    if self.else_block:
+        return self.else_block.eval(c)
+
+    return None
+
+
+def pushable_eval(self, c):
+    if self.var:
+        return c.get_name(self.var)
+    elif self.expr:
+        return self.expr.e.eval(c)
+    elif self.function:
+        logging.debug(c)
+        self.function.c = c
+        self.function.arg_dict = self.function.args.eval(c)
+        return self.function
+    elif hasattr(self.rawval, 'sentences'):
+        return [s.eval(c) for s in self.rawval.sentences]
+    elif hasattr(self.rawval, 'keys'):
+        return {k.eval(c): v.eval(c) for (k, v) in zip(self.rawval.keys, self.rawval.values)}
+    else:
+        return self.rawval
+
+
+def while_eval(self, c):
+    last_eval = None
+    while True:
+        res = self.condition.eval(c)
+        if res is None:
+            raise Exception('must have a result')
+        elif res:
+            last_eval = self.block.eval(c)
+        else:
+            break
+    return last_eval
+
+
+def types_eval(self, c):
+    return {entry.name: c.get_name(entry.type) for entry in self.varsWithType}
+
+
+def for_eval(self, c):
+    last_eval = None
+    for elem in self.sentence.eval(c):
+        if not self.var:
+            c.push_type(elem)
+        elif self.var and self.var != '_':
+            c.push_name(self.var, elem)
+        last_eval = self.block.eval(c)
+    return last_eval
+
+
+def block_eval(self, c):
+    for paragraph in self.paragraphs:
+        debug(paragraph)
+        for line in paragraph.lines:
+            debug(line)
+            try:
+                res = line.expr.eval(c)
+                if res is None:
+                    assert not line.assignment
+                elif isinstance(line.expr, Sentence) and not line.assignment:
+                    # i.e. only push from a sentence by default
+                    # swallow things from ifs/whiles/fors to avoid noise
+                    c.push_type(res)
+                elif line.assignment and line.assignment.var != '_':
+                    c.push_name(line.assignment.var, res)
+            except Exception as e:
+                error(line.expr, e)
+
+        try:
+            c.result()  # validate each paragraph has at most one result
+        except Exception as e:
+            error(paragraph, e)
+
+    logging.debug('function result %s', c.result())
+    return c.result()
+
+
+def sentence_eval(self, c):
+    debug(self)
+    s_c = Context(c)
+    for instruction in self.instructions:
+        if instruction == '.':
+            logging.debug('before func: %s', s_c)
+            val = s_c.pop_type(Function).eval(s_c)
+            if val is not None:
+                s_c.push_type(val)
+            logging.debug('after func: %s', s_c)
+        else:
+            s_c.push_type(instruction.eval(c))
+
+    return s_c.result()
+
+
+for cls_name, cls in model.namespaces[None].items():
+    eval_name = f'{cls_name.lower()}_eval'
+    if eval_name in vars():
+        cls.eval = vars()[eval_name]
+    globals()[cls_name] = cls
+
+
+def make_builtin(fn, args_dict):
+    r = Function()
+    # TODO should have same lookup rules as normal Function.
+    # also, this is wacky.
+    r.eval = lambda s_c: fn(*(s_c.pop_type(t) for (_, t) in args_dict.items()))
+    return r
 
 
 def debug(m, s="info"):
@@ -353,7 +305,7 @@ class Context:
     def copy(self):
         return Context(self.parent, self.n_v.copy(), self.t_v.copy(), self.args.copy(), self.args_dupe_types.copy())
 
-    def new_function(self, s_c, args):
+    def new_function_context(self, s_c, args):
         """
         functions on eval take a copy of the paragraph level context available at definition time
         (in self.c)
@@ -394,17 +346,17 @@ def main(fname, *args):
     def p(s):
         print(s)
     c = Context()
-    c.push_name('print_s', Function(builtin=print, arg_dict=dict(s=str)))
-    c.push_name('print_f', Function(builtin=print, arg_dict=dict(f=float)))
-    c.push_name('print_l', Function(builtin=print, arg_dict=dict(l=list)))
-    c.push_name('print_d', Function(builtin=print, arg_dict=dict(d=dict)))
+    c.push_name('print_s', make_builtin(print, dict(s=str)))
+    c.push_name('print_f', make_builtin(print, dict(f=float)))
+    c.push_name('print_l', make_builtin(print, dict(l=list)))
+    c.push_name('print_d', make_builtin(print, dict(d=dict)))
     c.push_name('string', str)
     c.push_name('number', float)
 
     program = model.model_from_file(fname) #, debug=DEBUG)
     program.eval(c)
 
-    #code.interact(local={**globals(), **locals()})
+    code.interact(local={**globals(), **locals()})
 
 
 if __name__ == '__main__':
